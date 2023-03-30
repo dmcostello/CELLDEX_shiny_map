b6310f4fb53a0c564d555e6f9523a4c0d711e5ce
David Costello <dcostel3@kent.edu>
2023-03-30 14:50:58 -0400

    New map with clickable points

--- app.R
+++ app.R
@@ -5,14 +5,19 @@
 
 library(shiny)
 library(leaflet)
+library(dplyr)
 Csites <- readRDS('./data/CELLDEX.rds')
+
+Csites <- mutate(Csites, cntnt=paste0('<strong>Site code: </strong>',part.str,
+                                            '<br><strong>Biome:</strong> ', biome_short.y,
+                                            '<br><strong>Decay rate (1/d):</strong> ', round(k,digits=3))) 
 
 #### --- USER INTERFACE --- ####
 
 ui <- navbarPage("CELLDEX",id="nav",
                  tabPanel("Interactive map",
                           sidebarLayout(
-                            mainPanel("map",
+                            mainPanel("maploc",
                             leafletOutput("map",width=600, height=800)),
                             sidebarPanel(
                               h2("Stream decomp"),
@@ -41,68 +46,18 @@ ui <- navbarPage("CELLDEX",id="nav",
 
 server <- function(input, output) {
   
-  # Draw the map
+  
   output$map <- renderLeaflet({
-    leaflet() %>%
-      addTiles() %>%
-      setView(lng = -81.36, lat = 41.15, zoom = 7)
+    leaflet(Csites) %>% 
+      addCircles(lng = ~longitude, lat = ~latitude) %>% 
+      addProviderTiles(providers$Esri.WorldTopoMap) %>%
+      addCircleMarkers(data = Csites, lat =  ~latitude, lng =~longitude,
+                       color = "#1b9e77",
+                       radius = 3, popup = ~as.character(cntnt),
+                       stroke = FALSE, fillOpacity = 0.8) %>%
+    setView(lng = -81.36, lat = 41.15, zoom = 6)
   })
   
-  # A reactive expression that returns the set of sites that are in bounds
-  # Was throwing errors so turned it off. App still runs fast without
-  #sitesInBounds <- reactive({
-    #if (is.null(input$map_bounds))
-      #return(Csites[FALSE,])
-    #bounds <- input$map_bounds
-    #latRng <- range(bounds$north, bounds$south)
-    #lngRng <- range(bounds$east, bounds$west)
-    
-    #subset(Csites,
-           #latitude >= latRng[1] & latitude <= latRng[2] &
-             #longitude >= lngRng[1] & longitude <= lngRng[2])
-  #})
-  
-  observe({
-    siteBy <- input$sites
-    
-    if (siteBy == "Cotton"){
-      radset <-  5
-      fillset <- "navy"}
-    else{radset <- 0
-    fillset <- "transparent"
-    }
-    
-    leafletProxy("map", data = Csites) %>%
-      clearShapes() %>%
-      addCircleMarkers(~longitude, ~latitude, layerId=~part.str,radius=radset,color=fillset)
-  })    
-    
-    # Generate a popup at the given location
-    showCELLDEX <- function(part.str, lat, lng) {
-      selectsite <- Csites[Csites$part.str == part.str,]
-      content <- "Works"
-        #as.character(tagList(
-        #tags$strong(HTML(sprintf("%s %s",
-        #                         selectsite$part.str, selectsite$biome_short.x
-        #))), tags$br(),
-        #sprintf("Decay rate (/d): %s", selectsite$k), tags$br(),
-      #))
-      leafletProxy("map") %>% addPopups(lng, lat, content, layerId = part.str)
-    }
-    
-    # When map is clicked, show a popup with site info
-    observe({
-      leafletProxy("map") %>% clearPopups()
-      event <- input$map_shape_click
-      if (is.null(event))
-        return()
-      
-      isolate({
-        showCELLDEX(event$id, event$lat, event$lng)
-      })
-    })
-    
-
   
 }
 
