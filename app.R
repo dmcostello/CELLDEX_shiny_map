077cf03699fadf88e8f3fb1608bdb0eaa0d125ac
David Costello <dcostel3@kent.edu>
2023-03-30 12:15:04 -0400

    Simplified the code and added in the k values. Added code for popup but not working.

--- app.R
+++ app.R
@@ -1,39 +1,41 @@
 #### --- GLOBAL --- ####
 
+#Adapted from SuperZip example https://shiny.rstudio.com/gallery/superzip-example.html
+#https://github.com/rstudio/shiny-examples/tree/main/063-superzip-example
+
 library(shiny)
 library(leaflet)
-Csites <- readRDS('./data/field.rds')
-Csites$part.stream <- with(Csites, paste(partnerid,stream))
+Csites <- readRDS('./data/CELLDEX.rds')
 
 #### --- USER INTERFACE --- ####
 
 ui <- navbarPage("CELLDEX",id="nav",
                  tabPanel("Interactive map",
-          
-                #Generate map                
-  leafletOutput("map"),
-                          
-          
-  absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-              draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                                        width = 330, height = "auto",
-              h2("Stream decomp"),
-              
-              radioButtons("type",label="Show decomposition rate of",
-                           choices = list("Cotton","Leaf litter")),
-              conditionalPanel("input.type == 'Leaf litter'",
-                               # Only prompt for litter type when selecting litter
-                               selectInput("leaf",label="Leaf genus",choices=list(
-                                 "Oak","Maple"))),
-              radioButtons("sites",label="Show sites",
-                           choices=list("None","Cotton","Leaf litter","Both"))
-              
-    ) 
-                 ),
-  conditionalPanel("false")
+                          sidebarLayout(
+                            mainPanel("map",
+                            leafletOutput("map",width=600, height=800)),
+                            sidebarPanel(
+                              h2("Stream decomp"),
+                              
+                              radioButtons("type",label="Show decomposition rate of",
+                                           choices = list("Cotton","Leaf litter")),
+                              conditionalPanel("input.type == 'Leaf litter'",
+                                               # Only prompt for litter type when selecting litter
+                                               selectInput("leaf",label="Leaf genus",choices=list(
+                                                 "Oak","Maple"))),
+                              radioButtons("sites",label="Show sites",
+                                           choices=list("None","Cotton","Leaf litter","Both"),
+                                           selected = "Cotton")
+                            )
+                          )
+          ),
+                      
+  tabPanel("Environmental data",
+           h1("Sliders here for environmental data")),
+  tabPanel("Substrate data",
+           h1("Sliders here for leaf traits"))
   )
                  
-
 
 #### --- SERVER LOGIC --- ####
 
@@ -43,21 +45,22 @@ server <- function(input, output) {
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      setView(lng = -93.85, lat = 37.45, zoom = 4)
+      setView(lng = -81.36, lat = 41.15, zoom = 7)
   })
   
   # A reactive expression that returns the set of sites that are in bounds
-  sitesInBounds <- reactive({
-    if (is.null(input$map_bounds))
-      return(Csites[FALSE,])
-    bounds <- input$map_bounds
-    latRng <- range(bounds$north, bounds$south)
-    lngRng <- range(bounds$east, bounds$west)
+  # Was throwing errors so turned it off. App still runs fast without
+  #sitesInBounds <- reactive({
+    #if (is.null(input$map_bounds))
+      #return(Csites[FALSE,])
+    #bounds <- input$map_bounds
+    #latRng <- range(bounds$north, bounds$south)
+    #lngRng <- range(bounds$east, bounds$west)
     
-    subset(Csites,
-           latitude >= latRng[1] & latitude <= latRng[2] &
-             longitude >= lngRng[1] & longitude <= lngRng[2])
-  })
+    #subset(Csites,
+           #latitude >= latRng[1] & latitude <= latRng[2] &
+             #longitude >= lngRng[1] & longitude <= lngRng[2])
+  #})
   
   observe({
     siteBy <- input$sites
@@ -71,10 +74,39 @@ server <- function(input, output) {
     
     leafletProxy("map", data = Csites) %>%
       clearShapes() %>%
-      addCircleMarkers(~longitude, ~latitude, layerId=~part.stream,radius=radset,color=fillset)
-  })
+      addCircleMarkers(~longitude, ~latitude, layerId=~part.str,radius=radset,color=fillset)
+  })    
+    
+    # Generate a popup at the given location
+    showCELLDEX <- function(part.str, lat, lng) {
+      selectsite <- Csites[Csites$part.str == part.str,]
+      content <- "Works"
+        #as.character(tagList(
+        #tags$strong(HTML(sprintf("%s %s",
+        #                         selectsite$part.str, selectsite$biome_short.x
+        #))), tags$br(),
+        #sprintf("Decay rate (/d): %s", selectsite$k), tags$br(),
+      #))
+      leafletProxy("map") %>% addPopups(lng, lat, content, layerId = part.str)
+    }
+    
+    # When map is clicked, show a popup with site info
+    observe({
+      leafletProxy("map") %>% clearPopups()
+      event <- input$map_shape_click
+      if (is.null(event))
+        return()
+      
+      isolate({
+        showCELLDEX(event$id, event$lat, event$lng)
+      })
+    })
+    
+
   
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server,options=list(display.mode = "showcase"))
+
+#runGitHub( "CELLDEX_shiny_map", "dmcostello")
